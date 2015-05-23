b7afa010cb0fac6b3447b60a25034f9e6f7c3593
Michael Williams <draringi@draringi.net>
2015-05-22 21:37:53 -0400

    added error catching for location service

--- js/location.js
+++ js/location.js
@@ -4,7 +4,7 @@ longitude = 0
 function get_location(){
 	if ( "geolocation" in navigator) {
 		console.log("if...")
-		navigator.geolocation.getCurrentPosition(set_location)
+		navigator.geolocation.getCurrentPosition(set_location, location_error)
 	} else {
 		add_error("No Location", "Your current browser does not support geolocation. As such this app will not work.")
 	}
@@ -14,6 +14,10 @@ function set_location(position) {
 	console.log("setting location")
 	latitude = position.coords.latitude;
 	longitude = positions.coords.longitude;
+}
+
+function location_error(err){
+	add_error("Error location self", err.message); 
 }
 
 get_location()
