8d05240b9b7affce54c41c64a6ddb18e0c5fd3ca
Michael Williams <draringi@draringi.net>
2015-05-23 12:30:10 -0400

    Added display.js, to switch between places of interest

---
+++ js/display.js
@@ -0,0 +1,47 @@
+var poi_index = 0;
+
+function set_poi(poi) {
+	$("#place_of_interest .place-name").html(poi.name)
+}
+
+
+function next_poi(){
+	if (poi_list.length == 0) {
+		return null;
+	}
+	if (poi_index >= (poi_list.length - 1)) {
+		return poi_list[poi_list.length - 1];
+	} else {
+		poi_index++;
+		return poi_list[poi_index];
+	}
+}
+
+function prev_poi(){
+	if (poi_list.length == 0) {
+		return null;
+	}
+	if (poi_index <= 0) {
+		return poi_list[0];
+	} else {
+		poi_index--;
+		return poi_list[poi_index];
+	}
+}
+
+function is_next_poi(){
+	return (poi_index < poi_list.length - 1);
+}
+
+function is_prev_poi(){
+	return (poi_list > 0);
+}
+
+document.onkeyup = function(e) {
+	e = e||window.event;
+	if (e.keyCode == "37") { //Left Arrow
+		set_poi(prev_poi());
+	} else if (e.keyCode == "39") { //Right Arrow
+		set_poi(next_poi());
+	}
+};
