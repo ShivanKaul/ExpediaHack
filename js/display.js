82ba19f5de6e6fefd2d7189f0fef893ab0055acf
ShivanKaul <shivan@omsignal.com>
2015-05-24 12:58:53 -0400

    Added link to uber sign up

--- js/display.js
+++ js/display.js
@@ -18,6 +18,7 @@
  var current_poi = null;
  var new_poi = false;
  var firstTime = true;
+ client_id = CLIENT_ID
 
 // talk = ["How about?", "Or maybe:", "Perhaps this?", "Consider going here, I hear it's great:", "This place is awesome I hear:", "My best friend loves this place:"]
 
@@ -30,6 +31,8 @@ function set_poi(poi) {
 	$("#link-yelp").attr("href", search);
 	current_poi = poi;
 	new_poi = true;
+	url_for_deeplinking = "https://m.uber.com/sign-up?client_id=" + client_id + "&dropoff_latitude=" + poi.coords[1] + "&dropoff_longitude=" + poi.coords[0] + "&pickup_latitude=" + latitude + "&dropoff_longitude=" + longitude
+	$("#book-button").attr("href", url_for_deeplinking);
 	// document.getElementById("loading").innerHTML = talk[Math.floor(Math.random() * talk.length)];
 	if (!firstTime) {
 		var elem = document.getElementById('loading');
